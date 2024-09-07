feb053e6079a203ec6a4e86db10399e3f6726943
umesh <umeshbltr9@gmail.com>
2024-09-07 22:15:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser"
+import dotenv from "dotenv";
+import cors from "cors";
+import route from "./routes/userRoute.js";
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace with your client's domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if your client needs to send cookies with the requests
+  };
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+dotenv.config();
+
+
+const PORT = process.env.PORT || 7000;
+const URL = process.env.MONGOURL;
+
+mongoose.connect(URL).then(()=>{
+
+    console.log("DB connected successfully");
+
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port: ${PORT}`);
+    })
+
+}).catch(error => console.log(error));
+
+
+app.use("/api", route);
